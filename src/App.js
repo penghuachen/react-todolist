9a779b3d73dffef97be117c303349c8c63cc5b70
Penghua <kids5346@gmail.com>
2020-07-27 15:56:14 +0800

    use scss file instead css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+
+// components
+import AddTask from './components/AddTask';
+import Header from './components/Header';
+
+
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <div className="container">
+        <Header />
+        <AddTask />
+      </div>
     </div>
   );
 }
