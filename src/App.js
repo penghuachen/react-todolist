6d8703dc4baca62d7349c63a571a3782c59d1ebb
Penghua <kids5346@gmail.com>
2020-07-28 19:48:21 +0800

    import TodoContent component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 // components
 import AddTask from './components/AddTask';
 import Header from './components/Header';
+import TodoContent from './components/TodoContent';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div className="app">
       <div className="container">
         <Header />
+        <TodoContent />
         <AddTask />
       </div>
     </div>
